331f6f3c9860c9dc9443b679b8c2b2dc4d5b687f
Meet Kataria <113782159+Meetlk@users.noreply.github.com>
2024-07-21 11:04:19 +0530

    axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || "ecomzy-e-commerce.vercel.app";
+const baseURL = process.env.REACT_APP_API_BASE_URL || "https://ecomzy-e-commerce.vercel.app";
 
 export default axios.create({
     baseURL: baseURL,
